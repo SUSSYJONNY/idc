8dbdee0237f00f5f8ce21dd60eed32d62753ca55
SUSSYJONNY <128854842+SUSSYJONNY@users.noreply.github.com>
2023-07-20 13:00:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ menuListArray = ["Pizza Vegetariana", "Pizza Mozzarella", "Pizza  Pepperonni", "
 function getMenu() {
     var htmldata = "";
     menuListArray.sort();
-    for (var i = 0; i < menuListArray.Lenght; i++) {
+    for (var i = 0; i < menuListArray.length; i++) {
         htmldata = htmldata + menuListArray[i] + '<br>';
     }
     document.getElementById("displayMenu").innerHTML = htmldata;
@@ -15,7 +15,7 @@ function addItem() {
     var item = document.getElementById("addItem").value;
     var imgTags = '<img id="im1" src="images/pizzaImg.png"/>'
     menuListArray.sort();
-    for (var i = 0; i < menuListArray.Lenght; i++) {
+    for (var i = 0; i < menuListArray.length; i++) {
         htmldata = htmldata + imgTags + menuListArray[i] + '<br>';
     }
     document.getElementById("displayAddedMenu").innerHTML = htmldata;
